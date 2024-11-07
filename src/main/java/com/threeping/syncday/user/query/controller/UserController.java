ed8be43d02e6252afebe2286b87251e1930acd1d
1etterh <1etterh.dev@gmail.com>
2024-11-07 11:27:22 +0900

    feature: spring 프로필 설정 + health check 추가

--- src/main/java/com/threeping/syncday/user/query/controller/UserController.java
+++ src/main/java/com/threeping/syncday/user/query/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.threeping.syncday.user.query.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @GetMapping("/health")
+    public String health(){
+        return "running on localhost:5000";
+    }
 }
