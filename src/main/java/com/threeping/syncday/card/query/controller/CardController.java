985ab93dff3f66a1f22b3a8377d3b96f714ffb5f
1etterh <1etterh.dev@gmail.com>
2024-11-07 14:30:53 +0900

    feat: Card Health Check 추가

--- src/main/java/com/threeping/syncday/card/query/controller/CardController.java
+++ src/main/java/com/threeping/syncday/card/query/controller/CardController.java
@@ -1,4 +1,12 @@
 package com.threeping.syncday.card.query.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/card")
 public class CardController {
+
+ 
 }
