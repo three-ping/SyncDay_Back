223c05c2431db19d2222e2a48cc7d39c1483b5a0
1etterh <1etterh.dev@gmail.com>
2024-11-12 11:27:04 +0900

    feat: Github에서 코드 받아옴

--- src/main/java/com/threeping/syncday/user/command/application/controller/OAuth2Controller.java
+++ src/main/java/com/threeping/syncday/user/command/application/controller/OAuth2Controller.java
@@ -4,9 +4,7 @@ import com.threeping.syncday.user.command.application.service.UserCommandService
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
@@ -23,5 +21,10 @@ public class OAuth2Controller {
         this.modelMapper = modelMapper;
     }
 
+    @GetMapping("/github")
+    public String github(@RequestParam String code) {
+        log.info("code: {}", code);
+        return null;
+    }
 
 }
