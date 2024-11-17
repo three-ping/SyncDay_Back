5d7f936551ca7965dda20753531598fd2a9276b7
이효진 <jin12lee@naver.com>
2024-11-17 11:14:35 +0900

    fix: front 연결 수정

--- src/main/java/com/threeping/syncday/chat/config/WebSocketConfig.java
+++ src/main/java/com/threeping/syncday/chat/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOrigins("http://localhost:5173")
                 .withSockJS();
     }
 
