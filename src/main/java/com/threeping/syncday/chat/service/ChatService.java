a197dc33e243e1fa41d1fa8204e056b217cdff28
jinjin0528 <jin12lee@naver.com>
2024-11-19 19:23:07 +0900

    fix: allowedMethods 수정

--- src/main/java/com/threeping/syncday/chat/service/ChatService.java
+++ src/main/java/com/threeping/syncday/chat/service/ChatService.java
@@ -39,16 +39,16 @@ public class ChatService {
         }
 
         //  채팅방 생성(멤버 필터)
-//        public ChatRoom createChatRoom(ChatRoomDTO chatRoomDTO){
-//            ChatRoom chatRoom = ChatRoom.builder ()
-//                    .roomId ( chatRoomDTO.getRoomId () )
-//                    .chatRoomName ( chatRoomDTO.getChatRoomName () )
-//                    .creatorId ( chatRoomDTO.getCreatorId () )
-//                    .createdAt ( chatRoomDTO.getCreatedAt () )
-//                    .memberCount ( chatRoomDTO.getMemberCount () )
-//                    .build ();
-//            return chatRoomRepository.save (chatRoom);
-//        }
+        public ChatRoom createChatRoom(ChatRoomDTO chatRoomDTO){
+            ChatRoom chatRoom = ChatRoom.builder ()
+                    .roomId ( chatRoomDTO.getRoomId () )
+                    .chatRoomName ( chatRoomDTO.getChatRoomName () )
+                    .creatorId ( chatRoomDTO.getCreatorId () )
+                    .createdAt ( chatRoomDTO.getCreatedAt () )
+                    .memberCount ( chatRoomDTO.getMemberCount () )
+                    .build ();
+            return chatRoomRepository.save (chatRoom);
+        }
 
         //  특정 채팅방 입장
         public ChatRoom joinChatRoom (String roomId, String userId) {
--- src/main/java/com/threeping/syncday/config/WebConfig.java
+++ src/main/java/com/threeping/syncday/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
-                        .allowedOrigins("GET", "POST", "OPTIONS", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
