f97338578c107d8664cee7f531bc8542a01e1d33
1etterh <1etterh.dev@gmail.com>
2024-11-21 16:09:25 +0900

    feat: 유저 아이디로 프로젝트와 워크스페이스 조회

---
+++ src/main/java/com/threeping/syncday/proj/query/aggregate/dto/ProjAndWorkspaceDTO.java
@@ -0,0 +1,39 @@
+package com.threeping.syncday.proj.query.aggregate.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.threeping.syncday.workspace.query.aggregate.WorkspaceDTO;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+public class ProjAndWorkspaceDTO {
+
+    @JsonProperty("proj_id")
+    Long projId;
+
+    @JsonProperty("proj_name")
+    String projName;
+
+    @JsonProperty("start_time")
+    Timestamp startTime;
+
+    @JsonProperty("end_time")
+    Timestamp endTime;
+
+    @JsonProperty("created_at")
+    Timestamp createdAt;
+
+    @JsonProperty("progress_status")
+    Byte progressStatus;
+
+    @JsonProperty("vcs_type")
+    String vcsType;
+
+    @JsonProperty("vcs_proj_url")
+    String vcsProjUrl;
+
+    @JsonProperty("workspaces")
+    List<WorkspaceDTO> workspaces;
+}
