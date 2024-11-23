4eb93741e29142c53fda75d2589ae3ba7371be07
1etterh <1etterh.dev@gmail.com>
2024-11-23 21:47:47 +0900

    feat: WorkspaceVO에 JsonProperty 추가

--- src/main/java/com/threeping/syncday/workspace/command/aggregate/vo/WorkspaceVO.java
+++ src/main/java/com/threeping/syncday/workspace/command/aggregate/vo/WorkspaceVO.java
@@ -1,5 +1,6 @@
 package com.threeping.syncday.workspace.command.aggregate.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.threeping.syncday.workspace.command.aggregate.entity.VcsType;
 import lombok.Data;
 
@@ -9,19 +10,28 @@ import java.sql.Timestamp;
 
 public class WorkspaceVO {
     /* Todo: 유효성 검사를 위해 유저아이디를 추가하여 VO로 생성 */
+
+    @JsonProperty("user_id")
     private Long userId;
 
+    @JsonProperty("workspace_id")
     private Long workspaceId;
 
+    @JsonProperty("workspace_name")
     private String workspaceName;
 
+    @JsonProperty("created_at")
     private Timestamp createdAt;
 
+    @JsonProperty("progress_status")
     private Byte progressStatus;
 
+    @JsonProperty("proj_id")
     private Long projId;
 
+    @JsonProperty("vcs_type")
     private VcsType vcsType;
 
+    @JsonProperty("vcs_repo_url")
     private String vcsRepoUrl;
 }
