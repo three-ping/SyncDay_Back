ade357d351db91b2437b06e07466a579c07259c4
Vorschlag-bit <https://github.com/Vorschlag-bit/testR.git>
2024-11-26 22:11:18 +0900

    commit: swagger를 위한 수정

--- src/main/java/com/threeping/syncday/user/infrastructure/elasticsearch/repository/UserSearchRepository.java
+++ src/main/java/com/threeping/syncday/user/infrastructure/elasticsearch/repository/UserSearchRepository.java
@@ -1,6 +1,7 @@
 package com.threeping.syncday.user.infrastructure.elasticsearch.repository;
 
 import com.threeping.syncday.user.infrastructure.elasticsearch.document.UserSearchDocument;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface UserSearchRepository extends ElasticsearchRepository<UserSearchDocument, Long> {
-    List<UserSearchDocument> findByNameContaining(String name);
+    @Query("{\"bool\": {\"should\": [{\"match\": {\"name\": \"?0\"}}, {\"match\": {\"teamName\": \"?0\"}}]}}")
+    List<UserSearchDocument> searchByKeyword(String keyword);
 }
--- src/main/java/com/threeping/syncday/user/query/dto/UserSearchResponse.java
+++ src/main/java/com/threeping/syncday/user/query/dto/UserSearchResponse.java
@@ -1,6 +1,7 @@
 package com.threeping.syncday.user.query.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -9,10 +10,16 @@ import lombok.ToString;
 @Builder
 @ToString
 public class UserSearchResponse {
+    @Schema(description = "유저 고유 번호(PK)", example = "11", type = "Long")
     private Long userId;
+    @Schema(description = "유저 이름", example = "장그래")
     private String name;
+    @Schema(description = "유저 이메일", example = "syncday1211@gmail.com")
     private String email;
+    @Schema(description = "유저 프로필사진", example = "dachshund.jpg")
     private String profileImage;
+    @Schema(description = "팀 이름", example = "개발팀")
     private String teamName;
+    @Schema(description = "직책", example = "대리")
     private String position;
 }
