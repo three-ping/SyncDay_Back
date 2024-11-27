3c0a9034b6f9313a30f4e0a2058207fa6eba4768
1etterh <1etterh.dev@gmail.com>
2024-11-27 09:22:36 +0900

    feat: ProjMemberController에 북마크 VO 추가

--- src/main/java/com/threeping/syncday/projmember/command/application/controller/AppProjMemberController.java
+++ src/main/java/com/threeping/syncday/projmember/command/application/controller/AppProjMemberController.java
@@ -1,4 +1,14 @@
 package com.threeping.syncday.projmember.command.application.controller;
 
+import com.threeping.syncday.common.ResponseDTO;
+import com.threeping.syncday.projmember.command.aggregate.entity.vo.ProjBookmarkVO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/proj-members")
 public class AppProjMemberController {
+    @PutMapping("/")
+    public ResponseDTO<?> bookmarkProj(@RequestBody ProjBookmarkVO bookmarkVO){
+        return null;
+    }
 }
