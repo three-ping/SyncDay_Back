2eca3548f27c249f241c64f9b370feaae9a6c292
1etterh <1etterh.dev@gmail.com>
2024-11-27 22:18:11 +0900

    feat: 카드 태그 DTO 생성

--- src/main/java/com/threeping/syncday/cardtag/query/controller/CardTagController.java
+++ src/main/java/com/threeping/syncday/cardtag/query/controller/CardTagController.java
@@ -1,4 +1,26 @@
 package com.threeping.syncday.cardtag.query.controller;
 
+import com.threeping.syncday.cardtag.query.service.CardTagService;
+import com.threeping.syncday.common.ResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/card-tags")
 public class CardTagController {
+
+    private final CardTagService cardTagService;
+
+    @Autowired
+    public CardTagController(CardTagService cardTagService) {
+        this.cardTagService = cardTagService;
+    }
+
+    @GetMapping("/{tagId}")
+    public ResponseDTO<?> findCardTagById(@PathVariable("tagId") Long tagId){
+        return ResponseDTO.ok(cardTagService.getCardTagById(tagId));
+    }
 }
---
+++ src/main/java/com/threeping/syncday/cardtag/query/service/CardTagServiceImpl.java
@@ -0,0 +1,12 @@
+package com.threeping.syncday.cardtag.query.service;
+
+import com.threeping.syncday.cardtag.query.aggregate.dto.CardTagDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardTagServiceImpl implements CardTagService {
+    @Override
+    public CardTagDTO getCardTagById(Long tagId) {
+        return null;
+    }
+}
