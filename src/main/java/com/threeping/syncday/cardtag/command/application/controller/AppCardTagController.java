270f3db04537d83db2f69419068c7be5e490e06b
1etterh <1etterh.dev@gmail.com>
2024-11-28 09:41:42 +0900

    feat: 카드 태그 command 패키지 생성

---
+++ src/main/java/com/threeping/syncday/cardtag/command/application/controller/AppCardTagController.java
@@ -0,0 +1,11 @@
+package com.threeping.syncday.cardtag.command.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/card-tags")
+public class AppCardTagController {
+
+
+}
