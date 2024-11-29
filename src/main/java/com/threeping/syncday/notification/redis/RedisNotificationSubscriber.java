2e0dde711dc4ff1a0136bef161fff1ed98c40bb7
dongkh9 <dongkh99@gamil.com>
2024-11-29 16:34:36 +0900

    refactor(notification): 알림 구조 수정 완료
    프론트와의 원활한 연결을 위한 heartbeat 추가 및 연결 로직 수정

--- src/main/java/com/threeping/syncday/notification/redis/RedisNotificationSubscriber.java
+++ src/main/java/com/threeping/syncday/notification/redis/RedisNotificationSubscriber.java
@@ -1,6 +1,5 @@
 package com.threeping.syncday.notification.redis;
 
-import com.threeping.syncday.notification.controller.NotificationSseController;
 import com.threeping.syncday.notification.service.NotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,19 +16,16 @@ public class RedisNotificationSubscriber implements MessageListener {
     @Autowired
     public RedisNotificationSubscriber(NotificationService notificationService) {
         this.notificationService = notificationService;
-        log.info("service in subscribe:{}",((Integer)System.identityHashCode(notificationService)).toString());
     }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        log.info("service in subscribe:{}",((Integer)System.identityHashCode(notificationService)).toString());
         String msg = new String(message.getBody());
         log.info("Received message: {}", msg);
         String[] parts = msg.split(":");
         Long userId = Long.valueOf(parts[1]);
         Long scheduleId = Long.valueOf(parts[2]);
         log.info("onMessage parsing userId: {}, scheduleId : {}",userId,scheduleId);
-        log.info("In subscriber emitters map: {}",notificationService.getEmitters().toString());
 
         notificationService.sendScheduleNotification(userId, scheduleId);
 
--- src/main/java/com/threeping/syncday/notification/service/NotificationService.java
+++ src/main/java/com/threeping/syncday/notification/service/NotificationService.java
@@ -3,7 +3,6 @@ package com.threeping.syncday.notification.service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.sql.Timestamp;
-import java.util.concurrent.ConcurrentHashMap;
 
 public interface NotificationService {
     SseEmitter createEmitter(Long userId);
@@ -12,5 +11,4 @@ public interface NotificationService {
 
     void sendScheduleNotification(Long userId, Long scheduleId);
 
-    ConcurrentHashMap<Long, SseEmitter> getEmitters();
     }
