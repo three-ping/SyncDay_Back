de5b7e585d1af20bb42f81ce767d66bea26ce527
1etterh <1etterh.dev@gmail.com>
2024-12-02 21:56:13 +0900

    feat: AppVcsController

--- src/main/java/com/threeping/syncday/vcs/command/application/controller/AppVcsController.java
+++ src/main/java/com/threeping/syncday/vcs/command/application/controller/AppVcsController.java
@@ -1,4 +1,23 @@
 package com.threeping.syncday.vcs.command.application.controller;
 
+import com.threeping.syncday.common.ResponseDTO;
+import com.threeping.syncday.vcs.command.application.service.AppVcsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vcs/org")
 public class AppVcsController {
+    private final AppVcsService appVcsService;
+
+    @Autowired
+    public AppVcsController(AppVcsService appVcsService) {
+        this.appVcsService = appVcsService;
+    }
+
+
 }
