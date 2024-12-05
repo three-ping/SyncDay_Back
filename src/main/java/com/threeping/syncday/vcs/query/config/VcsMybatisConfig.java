bf9184e0cede273aafeda1626f100d8ab9af769c
1etterh <1etterh.dev@gmail.com>
2024-12-05 19:22:14 +0900

    feat: Mapper Config 추가

--- src/main/java/com/threeping/syncday/vcs/query/config/VcsMybatisConfig.java
+++ src/main/java/com/threeping/syncday/vcs/query/config/VcsMybatisConfig.java
@@ -1,4 +1,15 @@
 package com.threeping.syncday.vcs.query.config;
 
+import com.threeping.syncday.vcs.query.repository.VcsOrgMapper;
+import com.threeping.syncday.vcs.query.repository.VcsUserMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@MapperScan(basePackages = "com.threeping.syncday.vcs.query.repository", annotationClass = Mapper.class)
+@Configuration
 public class VcsMybatisConfig {
+
+    private VcsOrgMapper vcsOrgMapper;
+    private VcsUserMapper vcsUserMapper;
 }
