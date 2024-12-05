4107c2d04342420ebb7a281da27c75ee7a9be308
1etterh <1etterh.dev@gmail.com>
2024-12-05 19:24:54 +0900

    feat: vcs 쿼리 서비스 생성

--- src/main/java/com/threeping/syncday/vcs/query/controller/VcsUserController.java
+++ src/main/java/com/threeping/syncday/vcs/query/controller/VcsUserController.java
@@ -1,9 +1,18 @@
 package com.threeping.syncday.vcs.query.controller;
 
+import com.threeping.syncday.vcs.query.service.VcsUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/vcs/user")
 public class VcsUserController {
+
+    private final VcsUserService vcsUserService;
+
+    @Autowired
+    public VcsUserController(VcsUserService vcsUserService) {
+        this.vcsUserService = vcsUserService;
+    }
 }
--- src/main/java/com/threeping/syncday/vcs/query/service/VcsUserServiceImpl.java
+++ src/main/java/com/threeping/syncday/vcs/query/service/VcsUserServiceImpl.java
@@ -1,4 +1,18 @@
 package com.threeping.syncday.vcs.query.service;
 
+import com.threeping.syncday.vcs.query.repository.VcsUserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class VcsUserServiceImpl implements VcsUserService {
+
+    private final VcsUserMapper vcsUserMapper;
+
+    @Autowired
+    public VcsUserServiceImpl(VcsUserMapper vcsUserMapper) {
+        this.vcsUserMapper = vcsUserMapper;
+    }
 }
