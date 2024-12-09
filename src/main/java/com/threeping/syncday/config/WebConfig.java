1affa035ac0eeda2a7fb0375493253dc0ebaf448
Vorschlag-bit <https://github.com/Vorschlag-bit/testR.git>
2024-12-09 15:58:31 +0900

    final

--- src/main/java/com/threeping/syncday/config/WebConfig.java
+++ src/main/java/com/threeping/syncday/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","https://1bda-203-234-103-13.ngrok-free.app")
+                        .allowedOrigins("http://syncday-front-env.eba-mgj2hqmj.ap-northeast-2.elasticbeanstalk.com","https://1bda-203-234-103-13.ngrok-free.app")
                         .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
