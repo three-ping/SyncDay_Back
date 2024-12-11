7b708b72fd96f9c1d569a4e8217238cab9b665cf
Vorschlag-bit <https://github.com/Vorschlag-bit/testR.git>
2024-12-11 12:31:09 +0900

    swagger

--- src/main/java/com/threeping/syncday/config/SwaggerConfiguration.java
+++ src/main/java/com/threeping/syncday/config/SwaggerConfiguration.java
@@ -2,6 +2,8 @@ package com.threeping.syncday.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OpenApiCustomizer;
@@ -9,6 +11,8 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
 
 @OpenAPIDefinition(info = @Info(title="SyncDay API 명세서",
                                 description = "SyncDay API 명세서",
@@ -27,6 +31,17 @@ public class SwaggerConfiguration {
                 .group("유저 관련 api")
                 .pathsToMatch(paths)
                 .addOpenApiCustomizer(buildSecurityOpenApi()).build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // HTTPS 서버 설정을 추가합니다
+        Server httpsServer = new Server()
+                .url("https://api.syncday.me")
+                .description("Production Server");
+
+        return new OpenAPI()
+                .servers(Arrays.asList(httpsServer));
     }
 
     @Bean
